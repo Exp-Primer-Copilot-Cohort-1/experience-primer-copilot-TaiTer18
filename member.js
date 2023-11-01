9a7a4bb52b7a7c78d7f22cb850ce4e5ab5e77696
TaiTer18 <122335428+TaiTer18@users.noreply.github.com>
2023-11-01 07:26:04 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    return {
+        restrict: 'E',
+        templateUrl: 'templates/member.html',
+        controller: 'MemberCtrl',
+        controllerAs: 'memberCtrl'
+    };
+}
+
+
+   
